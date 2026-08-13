186694140ac022ad550b852ab4fd8d26b4e1bb5b
Naim-087 <mdnaim2228@gmail.com>
2026-08-13 19:31:15 +0600

    JavaScript EventListener,DOM practise for final exam

---
+++ JavaScript/Array/array_object_destructing.js
@@ -0,0 +1,12 @@
+const student = {
+  name: 'Rahim',
+  age: 21,
+  department: 'CSE'
+};
+
+// Destructuring properties into variables
+const { name, age, department } = student;
+
+console.log(name);       // 'Rahim'
+console.log(age);        // 21
+console.log(department); // 'CSE'
